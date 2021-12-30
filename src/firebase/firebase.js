71e6af99ce0cd48d6210fbfdd84bd8e917467f2b
Jaiveer Talwar <72844076+jaiveertalwar@users.noreply.github.com>
2021-12-30 10:05:49 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //Your config values
+  apiKey: "AIzaSyCuwQOX1ybQ3BSk7OAPP_Y21ZEaoQkPcB8",
+  authDomain: "qlk-link.firebaseapp.com",
+  projectId: "qlk-link",
+  storageBucket: "qlk-link.appspot.com",
+  messagingSenderId: "234473233997",
+  appId: "1:234473233997:web:8886bf02e4dedbcbcf4604",
+  measurementId: "G-08HCSKXNH1"
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
